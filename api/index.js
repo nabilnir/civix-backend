e7a64f2483fd53008079532c79321ce069047add
nabilnir <nirarhan@gmail.com>
2025-12-11 18:22:42 +0600

    vercel deploy api added

---
+++ api/index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import { connectDB } from '../config/db.js';
+import authRoutes from '../routes/auth.js';
+import issuesRoutes from '../routes/issues.js';
+import usersRoutes from '../routes/users.js';
+import staffRoutes from '../routes/staff.js';
+import paymentsRoutes from '../routes/payments.js';
+import adminRoutes from '../routes/admin.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+  origin: [
+    'https://civix-com.web.app/',
+    'https://civix-backend-livid.vercel.app',
+    process.env.CLIENT_URL
+  ],
+  credentials: true
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send({ success: true, message: 'Civix Server is running!' });
+});
+
+app.use('/api/auth', authRoutes);
+app.use('/api/issues', issuesRoutes);
+app.use('/api/users', usersRoutes);
+app.use('/api/staff', staffRoutes);
+app.use('/api/payments', paymentsRoutes);
+app.use('/api/admin', adminRoutes);
+
+// Export handler for Vercel
+export default app;
